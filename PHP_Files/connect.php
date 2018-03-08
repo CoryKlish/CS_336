ccdad35bb3e1670c2654161c576dbdf878f609e5
Ken Chang <kchang711@yahoo.com>
2018-03-08 13:54:29 -0500

    Delete connect.php

--- PHP_Files/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbName = "Foods";
-$sqlUser = "CoryKlish";
-$sqlPass = "cory5510";
-$serverName = "cjk-cs336-2018.ccsvkmqvecdr.us-east-2.rds.amazonaws.com";
-$conn = mysqli_connect($serverName, $sqlUser, $sqlPass, $dbName);
-
-?>
