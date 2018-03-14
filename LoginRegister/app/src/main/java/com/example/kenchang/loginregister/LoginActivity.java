1ae0f4a20681f76fb5d3f50539a46d84116937fd
Elisa Orsini <elisaorsini@Elisas-MBP.home>
2018-03-14 03:03:24 -0400

    modified background of all pages and added navigation menu

--- LoginRegister/app/src/main/java/com/example/kenchang/loginregister/LoginActivity.java
+++ LoginRegister/app/src/main/java/com/example/kenchang/loginregister/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.kenchang.loginregister;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
--- LoginRegister/app/src/main/java/com/example/kenchang/loginregister/UserAreaActivity.java
+++ LoginRegister/app/src/main/java/com/example/kenchang/loginregister/UserAreaActivity.java
@@ -2,15 +2,22 @@ package com.example.kenchang.loginregister;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout; //For navigation menu
+    private ActionBarDrawerToggle mToggle; //For navigation menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
 
         //final EditText etUserName = (EditText) findViewById(R.id.etlastname);
         //final EditText etdob = (EditText) findViewById(R.id.etpassword);
@@ -28,5 +35,22 @@ public class UserAreaActivity extends AppCompatActivity {
         //Toast.makeText(getApplicationContext(),dob, Toast.LENGTH_SHORT).show();
         //etdob.setText(dob);
 
+
+        //for navigation bar:
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //works when any item on the action bar is tapped on
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
